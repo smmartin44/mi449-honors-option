ca2ce0019b256e0caec7201dcccba7eef6e01b70
Sophie Martin <mart2035@msu.edu>
2022-02-24 12:23:45 -0500

    changes for successful build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
