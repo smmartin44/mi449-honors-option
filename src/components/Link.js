8c767124cc3667281e6be35588253e4f01a3eacf
Sophie Martin <mart2035@msu.edu>
2022-04-18 17:08:15 -0400

    update home page, nav bar

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Link.css'
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React from 'react'
+import './Home.css'
+
 /* We simply can use an array and loop and print each user */
 const Home = () => {
   return (
-    <h1>Testing Home Page</h1>
-  );
-};
+    <div className='centered'>
+      <h1>Coffee Lovers Unite!</h1>
+      <p className='intro-paragraph'>We take our coffee very seriously - only using fresh beans daily sets us apart from the big coffee chains. Our syrups are made in house and our milk is sourced from local vendors. Come by and visit to see what we are all about!</p>
+      <div>
+        <div className='images'>
+          <div className='image-p-pair'>
+            <img className='round-img' src='assets/beans.jpg' />
+            <p>High quality coffee beans sourced internationally every month</p>
+          </div>
+          <div className='image-p-pair'>
+            <img className='round-img' src='assets/coffeecups.jpg' />
+            <p>Barista-crafted coffee drinks - all fan favorites!</p>
+          </div>
+          <div className='image-p-pair'>
+            <img className='round-img' src='assets/espresso.jpg' />
+            <p>Ability to customize all drinks to YOUR preferences!</p>
+          </div>
+        </div>
+      </div>
+      <p className='closing-paragraph'>Come visit us downtown in store, or check out our merchandise in our online store!</p>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
